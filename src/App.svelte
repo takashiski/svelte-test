284c08ad920908fa0d9c0807abbc74f99172f94d
takashiski <t_watanabe@denkiyagi.jp>
2021-11-05 12:21:10 +0900

    出せるカードを見えるように変更

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
   import { Client } from "boardgame.io/client";
   import { generateCucamber } from "./Cucamber";
-  import type { ICard, IState } from "./types/types";
+  import type { ICard, IState, IG } from "./types/types";
   let cards: ICard[] = [];
   let currentPlayerId = "";
   let layouts: (ICard | null)[] = [];
-  let G:IG = null;
+  let G: IG = null;
   const client = Client({
     game: generateCucamber(3),
     numPlayers: 3,
@@ -41,7 +41,7 @@
     how to build Svelte apps.
   </p> -->
   <h2>情報</h2>
-  <hr>
+  <hr />
   <table>
     <tr>
       <td>ラウンド</td>
@@ -53,7 +53,7 @@
       <td>{G.round.count}</td>
       <td>{G.trickCount}</td>
       <td>{G.prevTrick.winner}</td>
-      <td>{G.trick.biggest!=null?G.trick.biggest.num:""}</td>
+      <td>{G.trick.biggest != null ? G.trick.biggest.num : ""}</td>
     </tr>
   </table>
 
@@ -73,7 +73,7 @@
             🥛
           {/each} -->
           <!-- {#each {length:player.cucamber%5} as _,i } -->
-          {#each {length:player.cucamber} as _,i}
+          {#each { length: player.cucamber } as _, i}
             🍼
           {/each}
         </td>
@@ -84,7 +84,6 @@
       {#each G.players as player}
         <td>{player.hand.length}</td>
       {/each}
-
     </tr>
     <tr>
       <td>場札</td>
@@ -100,12 +99,24 @@
   <table>
     <tr>
       {#each cards as c, i (c)}
-        {#if G.trick.biggest==null ||  G.trick.biggest.num <= c.num }
-          <td class="card discardable-bigger" on:click={discard} index={i} number={c.num} value={c.num}>{c.num}</td >
-        {:else if c.num === cards.reduce((p,c)=>p.num>c.num?c:p).num}
-          <td class="card discardable-min" on:click={discard} index={i} number={c.num} value={c.num}>{c.num}</td >
+        {#if G.trick.biggest == null || G.trick.biggest.num <= c.num}
+          <td
+            class="card discardable-bigger"
+            on:click={discard}
+            index={i}
+            number={c.num}
+            value={c.num}>{c.num}</td
+          >
+        {:else if c.num === cards.reduce((p, c) => (p.num > c.num ? c : p)).num}
+          <td
+            class="card discardable-min"
+            on:click={discard}
+            index={i}
+            number={c.num}
+            value={c.num}>{c.num}</td
+          >
         {:else}
-          <td class="card" index={i} number={c.num} value={c.num}>{c.num}</td >
+          <td class="card" index={i} number={c.num} value={c.num}>{c.num}</td>
         {/if}
         <!-- <Card bind:nu={c.num} bind:clicked={c.clicked}/> -->
       {/each}
@@ -136,19 +147,19 @@
   table {
     align-content: center;
   }
-  td{
-    height:50px;
-    border:solid 1px;
+  td {
+    height: 50px;
+    border: solid 1px;
   }
   .card {
     border: solid 1px;
     height: 140px;
     width: 100px;
   }
-  .discardable-bigger{
-    background-color:#ccffff;
+  .discardable-bigger {
+    background-color: #ccffff;
   }
-  .discardable-min{
+  .discardable-min {
     background-color: #ffccff;
   }
 </style>
