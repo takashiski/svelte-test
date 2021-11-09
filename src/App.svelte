7ab3545b0b06ee0ed9ee445c92a8394a1f542d1d
takashiski <takashiskibb@gmail.com>
2021-11-10 00:05:45 +0900

    for netlify

--- src/App.svelte
+++ src/App.svelte
@@ -14,8 +14,8 @@
   let numOfPlayers: number = 2;
 
   const {protocol,hostname,port} = window.location;
-  const serverUrl = `${protocol}//${hostname}:${port}`;
-  // const serverUrl = "https://vast-reaches-25264.herokuapp.com:8000";
+  // const serverUrl = `${protocol}//${hostname}:${port}`;
+  const serverUrl = "https://vast-reaches-25264.herokuapp.com:8000";
   // const serverUrl = "http://localhost:8000";
   const lobbyClient = new LobbyClient({ server: serverUrl });
   async function listGames() {
