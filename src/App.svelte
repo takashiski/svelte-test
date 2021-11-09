65ac60ded9c8ce5fca6d62afcf37e4ffd69e3274
takashiski <takashiskibb@gmail.com>
2021-11-10 02:06:23 +0900

    ローカルで試せるように調整

--- src/App.svelte
+++ src/App.svelte
@@ -3,7 +3,7 @@
   import { LobbyClient } from "boardgame.io/client";
   import Player from "./Player.svelte";
   import type { LobbyAPI } from "boardgame.io";
-import { identity, text } from "svelte/internal";
+  import { identity, text } from "svelte/internal";
   let playerId: string = "";
   let playerName: string = "";
   let matchId: string = "";
@@ -15,7 +15,10 @@ import { identity, text } from "svelte/internal";
   let numOfPlayers: number = 2;
 
   const { protocol, hostname, port } = window.location;
-  const serverUrl = `${protocol}//${hostname}:${port}`;
+  const serverUrl =
+    process.env.NODE_ENV == "production"
+      ? `${protocol}//${hostname}:${port}`
+      : "http://localhost:8000";
   // const serverUrl = "https://vast-reaches-25264.herokuapp.com";
   // const serverUrl = "http://localhost:8000";
   const lobbyClient = new LobbyClient({ server: serverUrl });
@@ -41,12 +44,17 @@ import { identity, text } from "svelte/internal";
       numPlayers: 2,
     });
     matchId = matchID;
+    console.log(`create new room ${matchID}`);
   }
   async function setMatchId() {
     matchId = tempMatchId.toString();
   }
   async function join() {
-    playerId="0";
+    console.log("join");
+    playerId = "0";
+    console.log(playerId);
+    console.log(matchId);
+    console.log(playerName);
     const { playerCredentials } = await lobbyClient.joinMatch(
       "Cucamber",
       matchId,
@@ -58,15 +66,18 @@ import { identity, text } from "svelte/internal";
     console.log(playerCredentials);
     credentials = playerCredentials;
   }
-  async function seated(match:LobbyAPI.Match, playerId:string){
+  async function seated(match: LobbyAPI.Match, playerId: string) {
+    matchId=match.matchID;
     console.log(match);
     console.log(playerId);
-    const {playerCredentials} = await lobbyClient.joinMatch(match.gameName,
-    match.matchID,
-    {
-      playerID:playerId,
-      playerName: playerName
-    });
+    const { playerCredentials } = await lobbyClient.joinMatch(
+      match.gameName,
+      match.matchID,
+      {
+        playerID: playerId,
+        playerName: playerName,
+      }
+    );
     console.log(playerCredentials);
     credentials = playerCredentials;
   }
@@ -74,19 +85,23 @@ import { identity, text } from "svelte/internal";
 
 <!-- <button on:click={listGames}>ゲーム一覧</button> -->
 <div>
-
-    プレイヤー名：<input type="text" bind:value={playerName} />
+  プレイヤー名：<input type="text" bind:value={playerName} />
   <button on:click={listMatches}>部屋一覧</button><br />
   {#if matches != null}
     {#each matches as match}
       {match.gameName}<br />
-      部屋ID：<input type="text" value={match.matchID}/><br />
+      部屋ID：<input type="text" value={match.matchID} /><br />
       参加者一覧
       {#each match.players as player}
         {#if player.isConnected == true}
           - {player.id} : {player.name}<br />
         {:else}
-          {player.id} : <button on:click={()=>{seated(match,player.id.toString())}}>ここに座る</button> <br />
+          {player.id} :
+          <button
+            on:click={() => {
+              seated(match, player.id.toString());
+            }}>ここに座る</button
+          > <br />
         {/if}
       {/each}
     {/each}
