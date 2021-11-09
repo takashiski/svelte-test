3ff38e172fe1d5a1b27f182edc368fec6738026a
takashiski <takashiskibb@gmail.com>
2021-11-10 00:15:47 +0900

    update for heroku with netlify

--- src/index.ts
+++ src/index.ts
@@ -31,8 +31,8 @@ const path = require("path");
 const server = Server({
   games: [Cucamber],
   origins: [Origins.LOCALHOST_IN_DEVELOPMENT,
-    "https://vast-reaches-25264.herokuapp.com/",
-    "https://optimistic-ritchie-26665a.netlify.app/"]
+    "https://vast-reaches-25264.herokuapp.com",
+    "https://optimistic-ritchie-26665a.netlify.app"]
   // db:database,
   // origins:Origins.LOCALHOST,
   // localhost:["http://localhost"],
