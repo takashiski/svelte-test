dd61c77a5b4707cab16db5f268f6f48133067a25
takashiski <takashiskibb@gmail.com>
2021-11-14 16:07:45 +0900

    formatter

--- src/Match.svelte
+++ src/Match.svelte
@@ -1,39 +1,37 @@
 <script lang="ts">
-import type { LobbyAPI } from "boardgame.io";
-import type { LobbyClient } from "boardgame.io/dist/types/packages/client";
-import { ActivePlayers } from "boardgame.io/dist/types/packages/core";
-import Player from "./Player.svelte";
-import type { MatchingData } from "./types/lobbyTypes";
+  import type { LobbyAPI } from "boardgame.io";
+  import type { LobbyClient } from "boardgame.io/dist/types/packages/client";
+  import { ActivePlayers } from "boardgame.io/dist/types/packages/core";
+  import Player from "./Player.svelte";
+  import type { MatchingData } from "./types/lobbyTypes";
 
-
-  export let lobbyClient:LobbyClient;
-  export let matchingData:MatchingData;
-  export let match:LobbyAPI.Match;
-  export let playerName:string;
-
+  export let lobbyClient: LobbyClient;
+  export let matchingData: MatchingData;
+  export let match: LobbyAPI.Match;
+  export let playerName: string;
 </script>
 
 <div class="match">
-
-ゲームの種類：{match.gameName}<br/>
-部屋ID:<input type="text" value={match.matchID}/>{match.players.filter((p)=>p.name).length}/{match.players.length}<br/>
-参加者一覧<br/>
-<ul>
-
-{#each match.players as player}
-<li>
-{#if player.name}
-{player.id} : {player.name} {player.isConnected}
-{:else}
-{player.id}
-{/if}
-</li>
-{/each}
-</ul>
+  ゲームの種類：{match.gameName}<br />
+  部屋ID:<input type="text" value={match.matchID} />{match.players.filter(
+    (p) => p.name
+  ).length}/{match.players.length}<br />
+  参加者一覧<br />
+  <ul>
+    {#each match.players as player}
+      <li>
+        {#if player.name}
+          {player.id} : {player.name} {player.isConnected}
+        {:else}
+          {player.id}
+        {/if}
+      </li>
+    {/each}
+  </ul>
 </div>
 
 <style>
-  .match{
-    border:1px solid;
+  .match {
+    border: 1px solid;
   }
 </style>
